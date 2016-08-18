86ae93677a73aa82d6ca54983481006eaaffd92b
shangfeiSF <tony20100125@126.com>
2016-08-18 22:18:32 +0800

    dev config

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,41 +6,47 @@ var options = {
   port: 8080
 }
 
-function makeClean() {
+function makeConfig() {
   var mainDir = path.join(process.cwd(), 'main')
 
-  var clean = []
+  var config = {
+    copy: {},
+    clean: {}
+  }
 
   fs.readdirSync(mainDir)
     .filter(function (dir) {
       return fs.statSync(path.join(mainDir, dir)).isDirectory()
     })
     .forEach(function (dir) {
-      var dir = path.join(mainDir, dir, 'build')
-      clean.push(dir)
+      config.copy[dir] = {
+        files: [
+          {
+            cwd: './main/' + dir,
+            src: '**/*',
+            dest: './_build_/' + dir,
+            expand: true
+          },
+        ]
+      }
+
+      config.clean[dir] = [path.join(mainDir, dir, 'build')]
     })
 
-  return clean
+  return config
 }
 
 module.exports = function (grunt) {
+  var config = makeConfig()
+
   grunt.initConfig({
     options: options,
 
     pkg: grunt.file.readJSON('package.json'),
 
-    copy: {
-      files: [
-        {
-          cwd: './main',
-          src: '**/*',
-          dest: './_build_',
-          expand: true
-        },
-      ]
-    },
+    copy: config.copy,
 
-    clean: makeClean(),
+    clean: config.clean,
 
     connect: {
       server: {
@@ -62,7 +68,12 @@ module.exports = function (grunt) {
   matchdep.filterDev('grunt-*').forEach(grunt.loadNpmTasks)
 
   grunt.registerTask('default', ['connect', 'open', 'watch'])
-  grunt.registerTask('build', ['copy'])
-  grunt.registerTask('clear', ['clean'])
 
+  grunt.registerTask('build', '', function (taskname) {
+    grunt.task.run('copy:' + taskname)
+  })
+
+  grunt.registerTask('clear', '', function (taskname) {
+    grunt.task.run('clean:' + taskname)
+  })
 }
